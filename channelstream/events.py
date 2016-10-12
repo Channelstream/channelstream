a77c9919de4ae398874c2e68409d3494e2db31b1
Marcin Lulek <info@webreactor.eu>
2016-10-13 00:44:37 +0200

    headers: allow proxy prefix rewrites

--- channelstream/events.py
+++ channelstream/events.py
@@ -4,7 +4,16 @@ from pyramid.events import subscriber
 
 @subscriber(NewRequest)
 def mysubscriber(event):
-    scheme = event.request.headers.get('HTTP_X_SCHEME', '')
-    scheme = event.request.headers.get('HTTP_X_URL_SCHEME', scheme)
+    # rewrite prefix if X-Script-Name header is present
+    script_name = event.request.headers.get('X_SCRIPT_NAME', '')
+    if script_name:
+        event.request.environ['SCRIPT_NAME'] = script_name
+        path_info = event.request.environ['PATH_INFO']
+        if path_info.startswith(script_name):
+            event.request.environ['PATH_INFO'] = path_info[len(script_name):]
+
+    # make sure http/https can be controller
+    scheme = event.request.headers.get('X_SCHEME', '')
+    scheme = event.request.headers.get('X_URL_SCHEME', scheme)
     if scheme:
         event.request.environ['wsgi.url_scheme'] = scheme
