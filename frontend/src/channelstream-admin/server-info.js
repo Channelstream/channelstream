1de2d25410a0fab24b2de19a7e3cb220bf8b0e01
Marcin Lulek <info@webreactor.eu>
2018-05-18 03:10:11 +0200

    change admin looks

--- frontend/src/channelstream-admin/server-info.js
+++ frontend/src/channelstream-admin/server-info.js
@@ -1,9 +1,8 @@
 import {LitElement, html} from '@polymer/lit-element';
-import { repeat } from 'lit-html/lib/repeat.js';
+import {repeat} from 'lit-html/lib/repeat.js';
 import '@polymer/iron-collapse/iron-collapse.js';
 import '@polymer/iron-icons/iron-icons.js';
 import '@polymer/iron-icons/social-icons.js';
-import '@polymer/paper-badge/paper-badge.js';
 import '@polymer/paper-button/paper-button.js';
 import '@polymer/paper-card/paper-card.js';
 import '@polymer/paper-tooltip/paper-tooltip.js';
@@ -16,7 +15,60 @@ import {actions as currentActions} from './redux/current_actions';
 
 class ServerInfo extends LitElement {
 
-    _render({channels, serverStats}) {
+    _render({channels, serverStats, selectedChannel}) {
+
+        let channelInfo = html`No channel selected`;
+
+        if (selectedChannel) {
+            let channel = channels.filter((i) => i.uuid === selectedChannel)[0];
+
+            channelInfo = html`
+                        <paper-card heading="channel: ${channel.name}" class$="channel-${channel.uuid}">
+            <div class="card-actions">
+                <span>
+                <paper-button raised toggles on-tap=${(e) => this.toggleHistory(channel.uuid)}">
+                    <iron-icon icon="icons:history"></iron-icon>History</paper-button>
+                <paper-tooltip position="top" animation-delay="0">Shows this channels history</paper-tooltip>
+                </span>
+                <span>
+                <paper-button raised toggles on-tap=${(e) => this.toggleUsers(channel.uuid)}">
+                    <iron-icon icon="social:people-outline"></iron-icon>Users</paper-button>
+                <paper-tooltip position="top" animation-delay="0">Shows currently connected users</paper-tooltip>
+                </span>
+            </div>
+                <div class="card-content">
+                    <ul>
+                        <li><strong>Long name</strong>: ${channel.long_name}</li>
+                        <li><strong>last active</strong>: ${channel.last_active}</li>
+                        <li><strong>Total connections</strong>: ${channel.total_connections}</li>
+                        <li><strong>Total users</strong>: ${channel.total_users}</li>
+                    </ul>
+                    <p><strong>Config</strong></p>
+                    <app-debug data="${channel.settings}"></app-debug>
+
+                    <iron-collapse class$="channel-history-${channel.uuid}">
+                        <div class="history-holder">
+                            <strong>Message history:</strong>
+                            ${channel.history.map((item) => html`
+                            <app-debug data="${item}"></app-debug>
+                            `)}
+                        </div>
+                    </iron-collapse>
+
+                    <iron-collapse class$="channel-users-${channel.uuid}">
+                        <div class="users-holder">
+                            <strong>Connected users:</strong>
+                            ${channel.users.map((item) => html`
+                            <div>${item}</div>
+                            `)}
+                        </div>
+                    </iron-collapse>
+
+            </div>
+            </paper-card>
+            `
+        }
+
         return html`
         <style>
             .server-stat {
@@ -56,7 +108,23 @@ class ServerInfo extends LitElement {
             .history-holder {
                 padding-top: 20px
             }
-
+            
+            .badge{
+                font-weight: bold;
+                border: 1px solid #eeeeee;
+                display: inline-block;
+                border-radius: 15px;
+                padding: 10px 15px;
+            }
+            .flex-parent{
+                 display: flex;
+            }
+            .column-left{
+                width: 300px;
+            }
+            .column-right{
+                flex-grow: 1;
+            }
         </style>
 
         <div class="server-stat">
@@ -64,78 +132,44 @@ class ServerInfo extends LitElement {
         </div>
         <div class="server-stat">
             Unique users remembered
-            <paper-badge label="${serverStats.remembered_user_count}"></paper-badge>
+            <div class="badge">${serverStats.remembered_user_count}</div>
         </div>
         <div class="server-stat">
             Unique users connected
-            <paper-badge label="${serverStats.unique_user_count}"></paper-badge>
+            <div class="badge">${serverStats.unique_user_count}</div>
         </div>
         <div class="server-stat">
             Total connections
-            <paper-badge label="${serverStats.total_connections}"></paper-badge>
+            <div class="badge">${serverStats.total_connections}</div>
         </div>
 
         <div class="server-stat">
             Total channels
-            <paper-badge label="${serverStats.total_channels}"></paper-badge>
+            <div class="badge">${serverStats.total_channels}</div>
         </div>
 
         <div class="server-stat">
             Messages since start
-            <paper-badge label="${serverStats.total_unique_messages}"></paper-badge>
+            <div class="badge">${serverStats.total_unique_messages}</div>
         </div>
 
         <div class="server-stat">
             All frames sent
-            <paper-badge label="${serverStats.total_messages}"></paper-badge>
+            <div class="badge">${serverStats.total_messages}</div>
         </div>
 
+        <div class="flex-parent">
+            <div class="column-left">
+            <ul>
             ${repeat(channels, (channel) => channel.name, (channel, index) => html`
-            <paper-card heading="channel: ${channel.name}" class$="channel-${channel.uuid}">
-                <div class="card-content">
-                    <ul>
-                        <li><strong>Long name</strong>: ${channel.long_name}</li>
-                        <li><strong>last active</strong>: ${channel.last_active}</li>
-                        <li><strong>Total connections</strong>: ${channel.total_connections}</li>
-                        <li><strong>Total users</strong>: ${channel.total_users}</li>
-                    </ul>
-                    <p><strong>Config</strong></p>
-                    <app-debug data="${channel.settings}"></app-debug>
-
-                    <iron-collapse class$="channel-history-${channel.uuid}">
-                        <div class="history-holder">
-                            <strong>Message history:</strong>
-                            ${channel.history.map((item) => html`
-                            <app-debug data="${item}"></app-debug>
-                            `)}
-                        </div>
-                    </iron-collapse>
-
-                    <iron-collapse class$="channel-users-${channel.uuid}">
-                        <div class="users-holder">
-                            <strong>Connected users:</strong>
-                            ${channel.users.map((item) => html`
-                            <div>${item}</div>
-                            `)}
-                        </div>
-                    </iron-collapse>
-
-            </div>
-            <div class="card-actions">
-                <span>
-                <paper-button raised toggles on-tap=${(e) => this.toggleHistory(channel.uuid)}">
-                    <iron-icon icon="icons:history"></iron-icon>History</paper-button>
-                <paper-tooltip position="top" animation-delay="0">Shows this channels history</paper-tooltip>
-                </span>
-                <span>
-                <paper-button raised toggles on-tap=${(e) => this.toggleUsers(channel.uuid)}">
-                    <iron-icon icon="social:people-outline"></iron-icon>Users</paper-button>
-                <paper-tooltip position="top" animation-delay="0">Shows currently connected users</paper-tooltip>
-                </span>
-            </div>
-            </paper-card>
+            <li><paper-button on-click=${(e) => this.selectChannel(channel.uuid)}>${index + 1} - ${channel.name}</paper-button></li>
             `)}
-
+            </ul>
+            </div>
+            <div class="column-right">
+                ${channelInfo}
+            </div>
+        </div>
         `;
     }
 
@@ -145,13 +179,15 @@ class ServerInfo extends LitElement {
 
     static get properties() {
         return {
-            channels: {
-                type: Array
-            },
-            serverStats: {
-                type: Object
-            }
+            channels: Array,
+            serverStats: Object,
+            selectedChannel: String
         };
+    }
+
+    selectChannel(channelId) {
+        console.log('sc', channelId)
+        this.selectedChannel = channelId;
     }
 
     toggleHistory(index) {
