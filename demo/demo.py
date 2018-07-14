6ad4bdc3176ee37eb2d9b2fd4cd4efb0332cf202
Marcin Lulek <info@webreactor.eu>
2018-07-15 00:46:57 +0200

    fix admin

--- demo/demo.py
+++ demo/demo.py
@@ -226,7 +226,7 @@ class DemoViews(object):
     @view_config(match_param=["section=demo", "action=info"])
     def info(self):
         """gets information for the "admin" demo page"""
-        admin = 'admin'
+        admin = "admin"
         admin_secret = self.request.registry.settings["admin_secret"]
         basic_auth = HTTPBasicAuth(admin, admin_secret)
         result = make_server_request(
