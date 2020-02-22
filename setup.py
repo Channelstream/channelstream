9bc595c5e7176cce9874525bfce0c1f670ffa5ee
Marcin Lulek <info@webreactor.eu>
2020-02-22 18:56:50 +0100

    setip.py: update trove identifiers

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Communications :: Chat",
         "Topic :: Communications :: Conferencing",
